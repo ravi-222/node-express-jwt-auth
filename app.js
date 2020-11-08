81e7686a849307de6c6edd94dd77c04b9fa275d9
ravi-222 <ravindersinghanand22@gmail.com>
2020-11-09 01:57:13 +0530

    learning cookieparser

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
-
+const cookieParser = require('cookie-parser');
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
-
+app.use(cookieParser());
 // view engine
 app.set('view engine', 'ejs');
 
@@ -23,10 +23,13 @@ app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
 app.get('/set-cookies',(req,res) => {
-  res.setHeader('set-Cookie', 'newUser=true');
+  // res.setHeader('set-Cookie', 'newUser=true');
+  res.cookie('newUser', false);
+  res.cookie('isWorking', true);
   res.send('coookkiiieeeesss!!!');
 })
 
 app.get('/read-cookies', (req,res) => {
-  
+  const cookies = req.cookies;
+  res.json(cookies);
 })
