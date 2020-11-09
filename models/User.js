0079fb3181f8ff061c5a52ec96a8360715618b9c
ravi-222 <ravindersinghanand22@gmail.com>
2020-11-09 17:54:50 +0530

    user login part1

--- models/User.js
+++ models/User.js
@@ -25,6 +25,18 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
+userSchema.statics.login = async function(email, password){
+  const user = await this.findOne({email});
+  if(user){
+    const auth = await bcrypt.compare(password, user.password);
+    if(auth){
+      return user;
+    }
+    throw Error('incorrect passwrd');
+  }
+  throw Error('incorrect email')
+}
+
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
